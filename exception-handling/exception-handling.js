3bd69837534a18bbadcc9c4ffa3d52199ab8e4d0
choseonghwan91 <choseonghwan91@gmail.com>
2021-12-30 13:35:04 +0900

    [Add] dream coding class file add

--- exception-handling/exception-handling.js
+++ exception-handling/exception-handling.js
@@ -1,13 +1,13 @@
 // 1. throw 처리
 
-// function sum(x,y) {
-//   if(typeof x !== 'number' || typeof y !== 'number'){
-//     throw '숫자가 아닙니다'
-//   }
-//   return x + y;
-// }
+function sum(x, y) {
+  if (typeof x !== "number" || typeof y !== "number") {
+    throw "숫자가 아닙니다";
+  }
+  return x + y;
+}
 
-// console.log(sum('a',2));
+console.log(sum("a", 2));
 
 // 출력값 : answer Uncaught 숫자가 아닙니다
 
@@ -16,21 +16,21 @@
 
 // 2. 예외처리
 
-// function f2() {
-//   console.log("f2 start");
-//   throw "error";
-//   console.log("f2 end");
-// }
+function f2() {
+  console.log("f2 start");
+  throw "error";
+  console.log("f2 end");
+}
 
-// function f1() {
-//   console.log("f1 start");
-//   f2();
-//   console.log("f1 end");
-// }
+function f1() {
+  console.log("f1 start");
+  f2();
+  console.log("f1 end");
+}
 
-// console.log("will : f1");
-// f1();
-// console.log("did : f1");
+console.log("will : f1");
+f1();
+console.log("did : f1");
 
 // 출력값
 // will: f1
@@ -43,25 +43,25 @@
 // f2에서 에러가 발생 되고 끝났기 때문에 f1에 있는 f2() 호출 코드도 에러
 // 예외처리를 하지 않았기 때문에 f1도 error 가 되고, 똑같은 방식으로 메인도 에러를 받아 실행을 멈춘다
 
-// function f2() {
-//   console.log("f2 start");
-//   throw "error";
-//   console.log("f2 end");
-// }
+function f2() {
+  console.log("f2 start");
+  throw "error";
+  console.log("f2 end");
+}
 
-// function f1() {
-//   console.log("f1 start");
-//   try {
-//     f2();
-//   } catch (e) {
-//     console.log(e);
-//   }
-//   console.log("f1 end");
-// }
+function f1() {
+  console.log("f1 start");
+  try {
+    f2();
+  } catch (e) {
+    console.log(e);
+  }
+  console.log("f1 end");
+}
 
-// console.log("will : f1");
-// f1();
-// console.log("did : f1");
+console.log("will : f1");
+f1();
+console.log("did : f1");
 
 // 출력값
 // will :  f1
@@ -75,25 +75,25 @@
 // catch 를 사용해서 예외처리를 한다.
 // 예외처리가 되면 그 이후의 코드도 실행된다.
 
-// function f2() {
-//   console.log("f2 start");
-//   throw "error";
-//   console.log("f2 end");
-// }
+function f2() {
+  console.log("f2 start");
+  throw "error";
+  console.log("f2 end");
+}
 
-// function f1() {
-//   console.log("f1 start");
-//   f2();
-//   console.log("f1 end");
-// }
+function f1() {
+  console.log("f1 start");
+  f2();
+  console.log("f1 end");
+}
 
-// console.log("will : f1");
-// try {
-//   f1();
-// } catch (e) {
-//   console.log(e);
-// }
-// console.log("did : f1");
+console.log("will : f1");
+try {
+  f1();
+} catch (e) {
+  console.log(e);
+}
+console.log("did : f1");
 
 // 출력값
 // will : f1
@@ -106,25 +106,25 @@
 // 'f2 end'는 콘솔에 출력되지 않았고, f1 함수에서 예외 처리를 하지 않았기 때문에 'f1 end'도 출력 되지 않았다
 // 전역에서 f1함수를 예외처리 해두었기 때문에 'did: f1'은 출력된다
 
-// function f2() {
-//   console.log("f2 start");
-//   throw new Error("error");
-//   console.log("f2 end");
-// }
+function f2() {
+  console.log("f2 start");
+  throw new Error("error");
+  console.log("f2 end");
+}
 
-// function f1() {
-//   console.log("f1 start");
-//   f2();
-//   console.log("f1 end");
-// }
+function f1() {
+  console.log("f1 start");
+  f2();
+  console.log("f1 end");
+}
 
-// console.log("will : f1");
-// try {
-//   f1();
-// } catch (e) {
-//   console.log(e);
-// }
-// console.log("did : f1");
+console.log("will : f1");
+try {
+  f1();
+} catch (e) {
+  console.log(e);
+}
+console.log("did : f1");
 
 // 출력값
 // will : f1
@@ -139,21 +139,17 @@
 // new Error 을 사용해여 예외를 등록하면
 // 어떤 파일의 어떤 함수에서 에러가 발생했는지 표현해 주기 때문에 분석하기 좋음
 
-//
-//
-//
-
 // 3. promise 에서 예외처리
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3);
+wait(3);
 
 // 출력값
 // 3초 뒤에
@@ -162,19 +158,19 @@
 // promise 에서 reject를 실행해서 예외를 잡았는데,
 // 그걸 예외처리를 해주지 않아서 uncaught 에러가 발생함
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// try {
-//   wait(3);
-// } catch (e) {
-//   console.error(e);
-// }
+try {
+  wait(3);
+} catch (e) {
+  console.error(e);
+}
 
 // 출력값
 // 3초 뒤에
@@ -183,17 +179,17 @@
 // try catch 로 잡히지 않음
 // 왜냐면 비동기적인 코드이기 때문에 try catch가 지나고 예외 발생
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3).catch((e) => {
-//   console.log("1st catch", e);
-// });
+wait(3).catch((e) => {
+  console.log("1st catch", e);
+});
 
 // 출력값
 // 3초 뒤에
@@ -201,21 +197,21 @@
 
 // promise 에서 지원하는 catch로 예외를 잡아준다.
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3)
-//   .catch((e) => {
-//     console.log("1st catch", e);
-//   })
-//   .catch((e) => {
-//     console.log("2nd catch", e);
-//   });
+wait(3)
+  .catch((e) => {
+    console.log("1st catch", e);
+  })
+  .catch((e) => {
+    console.log("2nd catch", e);
+  });
 
 // 출력값
 // 3초 뒤에
@@ -227,22 +223,22 @@
 // 그래서 첫번째 catch에 있는 1st catch, e 를 콘솔에 출력하는것이 정상적으로 되었기 때문에
 // 두번째 .catch 는 동작하지 않음
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3)
-//   .catch((e) => {
-//     console.log("1st catch", e);
-//     throw e;
-//   })
-//   .catch((e) => {
-//     console.log("2nd catch", e);
-//   });
+wait(3)
+  .catch((e) => {
+    console.log("1st catch", e);
+    throw e;
+  })
+  .catch((e) => {
+    console.log("2nd catch", e);
+  });
 
 // 출력값
 // 3초 뒤에
@@ -253,26 +249,26 @@
 // 예외처리 한곳에서 할 작업을 정의하고 마지막에 throw 로 에러를 발생시켜주면
 // 두번째 catch 도 작동이 되서 작업을 마무리 할수 있음
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3)
-//   .then(
-//     () => {
-//       console.log("done!");
-//     },
-//     (e) => {
-//       console.log("1st catch", e);
-//     }
-//   )
-//   .catch((e) => {
-//     console.log("2nd catch", e);
-//   });
+wait(3)
+  .then(
+    () => {
+      console.log("done!");
+    },
+    (e) => {
+      console.log("1st catch", e);
+    }
+  )
+  .catch((e) => {
+    console.log("2nd catch", e);
+  });
 
 // 출력값
 // 3초 뒤에
@@ -282,27 +278,27 @@
 // .then 에 두번째 인자를 사용해서 예외처리가 가능
 // 역시나 .then 에서 예외처리를 했기 때문에 두번째 .catch 는 작동하지 않음
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("error!");
+    }, sec * 1000);
+  });
+}
 
-// wait(3)
-//   .then(
-//     () => {
-//       console.log("done!");
-//     },
-//     (e) => {
-//       console.log("1st catch", e);
-//       throw new Error("throw in Then");
-//     }
-//   )
-//   .catch((e) => {
-//     console.log("2nd catch", e);
-//   });
+wait(3)
+  .then(
+    () => {
+      console.log("done!");
+    },
+    (e) => {
+      console.log("1st catch", e);
+      throw new Error("throw in Then");
+    }
+  )
+  .catch((e) => {
+    console.log("2nd catch", e);
+  });
 
 // 출력값
 // 3초 뒤에
@@ -312,27 +308,23 @@
 // then의 예외처리에서 throw new Error 로 에러를 던져줘서
 // .catch 가 실행됨
 
-//
-//
-//
-
 // 3. async await 에서의 예외 처리
 
-// async function myAsyncFunc() {
-//   return "done!";
-// }
+async function myAsyncFunc() {
+  return "done!";
+}
 
-// function myPromiseFunc() {
-//   return new Promise((resolve, reject) => {
-//     resolve("done!");
-//   });
-// }
+function myPromiseFunc() {
+  return new Promise((resolve, reject) => {
+    resolve("done!");
+  });
+}
 
-// const result1 = myAsyncFunc();
-// console.log(result1);
+const result1 = myAsyncFunc();
+console.log(result1);
 
-// const result2 = myPromiseFunc();
-// console.log(result2);
+const result2 = myPromiseFunc();
+console.log(result2);
 
 // 출력값
 // Promise{<fulfilled>:'done!'}
@@ -341,21 +333,21 @@
 // async 로 함수를 선언하면 안에 프로미스가 생성됨
 // 위와 아래의 코드의 출력값이 같음
 
-// async function myAsyncFunc() {
-//   throw "myAsyncError!";
-// }
+async function myAsyncFunc() {
+  throw "myAsyncError!";
+}
 
-// function myPromiseFunc() {
-//   return new Promise((resolve, reject) => {
-//     reject("myPromiseError!");
-//   });
-// }
+function myPromiseFunc() {
+  return new Promise((resolve, reject) => {
+    reject("myPromiseError!");
+  });
+}
 
-// const result1 = myAsyncFunc();
-// console.log(result1);
+const result1 = myAsyncFunc();
+console.log(result1);
 
-// const result2 = myPromiseFunc();
-// console.log(result2);
+const result2 = myPromiseFunc();
+console.log(result2);
 
 // 출력값
 // Promist{<rejected>: 'myAsyncError!'}
@@ -367,23 +359,23 @@
 // async await 에서는 함수에서 에러를 발생시킨것과 마찬가지로
 // throw를 이용해서 에러를 발생시켜 준다
 
-// async function myAsyncFunc() {
-//   throw "myAsyncError!";
-// }
+async function myAsyncFunc() {
+  throw "myAsyncError!";
+}
 
-// function myPromiseFunc() {
-//   return new Promise((resolve, reject) => {
-//     reject("myPromiseError!");
-//   });
-// }
+function myPromiseFunc() {
+  return new Promise((resolve, reject) => {
+    reject("myPromiseError!");
+  });
+}
 
-// const result1 = myAsyncFunc().catch((e) => {
-//   console.error(e);
-// });
+const result1 = myAsyncFunc().catch((e) => {
+  console.error(e);
+});
 
-// const result2 = myPromiseFunc().catch((e) => {
-//   console.error(e);
-// });
+const result2 = myPromiseFunc().catch((e) => {
+  console.error(e);
+});
 
 // 출력값
 // myAsyncError!
@@ -392,21 +384,21 @@
 // 예외처리는 promise를 반환하기 때문에
 // async await 에서도 .catch 를 사용해서 처리해준다
 
-// function wait(sec) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve("done!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   wait(3);
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  wait(3);
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -415,21 +407,21 @@
 // wait의 3초를 기다리지 않고 바로 시간이 두개 다 출력됨
 // 이유는 wait 함수는 비동기 함수이기 때문에 다음코드가 바로 실행이 되는것
 
-// function wait(sec) {
-//   return new Promise((resolve) => {
-//     setTimeout(() => {
-//       resolve("done!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("done!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   await wait(3);
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  await wait(3);
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -439,21 +431,21 @@
 // await는 promise를 기다려 줄수 있게 만들어줌
 // 비동기함수를 처리하는 비동기처리가 된것
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("wait Error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("wait Error!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   await wait(3);
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  await wait(3);
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -462,25 +454,25 @@
 
 // wait 함수에서 에러가 발생했지만 예외처리를 해주지 않았기 때문에 uncaught 에러가 발생
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("wait Error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("wait Error!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   try {
-//     await wait(3);
-//   } catch (e) {
-//     console.error(e);
-//   }
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  try {
+    await wait(3);
+  } catch (e) {
+    console.error(e);
+  }
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -491,23 +483,23 @@
 // try catch 를 사용하여 예외처리를 해주었기 때문에 에러가 발생하고
 // 그 다음 코드인 시간이 출력 될수 있음
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("wait Error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("wait Error!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   await wait(3).catch((e) => {
-//     console.error(e);
-//   });
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  await wait(3).catch((e) => {
+    console.error(e);
+  });
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -518,24 +510,24 @@
 // 바로 .catch 를 사용해도 똑같은 결과가 나오지만,
 // 저 함수 값을 리턴할 경우는 문제가 있을수 있음
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("wait Error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("wait Error!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   console.log(new Date());
-//   const result = await wait(3).catch((e) => {
-//     console.error(e);
-//   });
-//   console.log(result);
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  console.log(new Date());
+  const result = await wait(3).catch((e) => {
+    console.error(e);
+  });
+  console.log(result);
+  console.log(new Date());
+}
 
-// const result = myAsyncFunc();
+const result = myAsyncFunc();
 
 // 출력값
 // 시간
@@ -546,26 +538,26 @@
 // wait 함수에서 에러가 발생하면 result는 .catch를 바라보고 있기 때문에
 // error 처리 되고 아무것도 리턴하지 않아서 undefined 가 출력됨
 
-// function wait(sec) {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       reject("wait Error!");
-//     }, sec * 1000);
-//   });
-// }
+function wait(sec) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject("wait Error!");
+    }, sec * 1000);
+  });
+}
 
-// async function myAsyncFunc() {
-//   오타console.log(new Date());
-//   const result = await wait(3).catch((e) => {
-//     console.error(e);
-//   });
-//   console.log(result);
-//   console.log(new Date());
-// }
+async function myAsyncFunc() {
+  오타console.log(new Date());
+  const result = await wait(3).catch((e) => {
+    console.error(e);
+  });
+  console.log(result);
+  console.log(new Date());
+}
 
-// myAsyncFunc().catch((e) => {
-//   console.log(e);
-// });
+myAsyncFunc().catch((e) => {
+  console.log(e);
+});
 
 // 출력값
 // ReferenceError: 오타console is not defined
